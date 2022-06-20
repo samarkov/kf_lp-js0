eb0e5163cfe4ec3527b91d58c4ff20527f07c11f
samarkov <konstantin.samarkov@gmail.com>
2022-06-20 22:40:54 +0300

    sync

--- js/index.js
+++ js/index.js
@@ -1,5 +1,18 @@
 //@ts-check
-/* задание 2 раздела 5.3 */
+
+let firstName= 'Ivan';
+let lastName = 'Ivanov';
+function getFullName(strings, firstNameExp, lastNameExp) {
+let str0 = strings[0];// "My  first name is " 
+let str1 = strings[1]; // " and the last " 
+return `${str0}${firstNameExp}${str1}${lastNameExp}`;
+}
+let result= getFullName`My first name is ${ firstName} and the last ${ lastName }`;
+console.log(result);
+
+
+
+/* задание 2 раздела 5.3 
 
 let inputValue;
 inputValue = true;
@@ -22,7 +35,7 @@ switch (typeof(inputValue)) {
         console.log(inputValue + ' - unknown');    
 }
 
-
+*/
 
 
 
