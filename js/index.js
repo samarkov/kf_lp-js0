29eee1eab2df4ff9c3b6146bf53f2c7f73abc07a
konstantin.samarkov <samarkov@mail.ru>
2022-06-22 20:44:28 +0300

    syn

--- js/index.js
+++ js/index.js
@@ -1,133 +1,169 @@
-/* Задание 3. раздела 5.3
-Дана строка. Необходимо вывести в консоль перевёрнутый вариант. Например, "Hello" -> "olleH". */
+// Задание 6.
 
-const inString = 'Hello';
-let outString = ' ';
+// Дан массив. Проверить, одинаковые ли элементы в массиве и вывести результат true или false в консоль.
+// Речь идёт не о двух рядом стоящих одинаковых элементах, а обо всех. Проверить, все ли элементы в массиве одинаковые.
 
-console.log(inString);
-
-for (let i=inString.length-1; i >= 0 ; i--) {
-    outString = outString + inString[i];
-} 
-
-console.log(outString);
-
-
-/* Задание 4. раздела 5.3
-   Записать в переменную случайное целое число в диапазоне [0; 100]. Используйте объект Math.*/
+// let randArray = [1, 2, 3, -3, 'arrrr', 'book'];
+let randArray = [1, ,1,1,1,1,1,1,1,1,1,1,1];
 
 
 
-
-/* задание 2 раздела 5.3 
-
-let inputValue;
-inputValue = true;
-// inputValue = 'dssdf';
-// inputValue = 20;
-// inputValue = null;
-
-
-switch (typeof(inputValue)) {
-    case 'string':
-        console.log(inputValue + ' - string');
-        break;
-    case 'boolean':
-        console.log(inputValue + ' - boolean');
-        break;
-    case 'number':
-        console.log(inputValue + ' - number');
-        break;
-    default:
-        console.log(inputValue + ' - unknown');    
-}
-
-*/
+            for (i = 0; i < randArray.length; i++) {
+                if ((randArray[i] != randArray[i + 1])) {
+                    console.log('False');
+                    break;
+                }
+                console.log('True');
+            }
 
 
 
+        // Задание 5.
 
-/* задание 1 раздел 5.3
-const inputString = prompt('Введите число:');
-const castInputValue = +inputString;
+        // Дан произвольный массив. Необходимо вывести количество элементов массива, затем 
+        //перебрать его и вывести в консоль каждый элемент массива.
 
-if (isNaN(castInputValue)) {
-    console.log('Упс, кажется вы ошиблись, "' + inputString + '" не является числом' );
-} else  {
-        const resultOdd = (castInputValue % 2 === 0);
-        if (castInputValue % 2 === 0) {
-            console.log('Число ' + inputString + ' - четное')
-        }  else {
-            console.log('Число ' + inputString + ' - нечетное');
+        // let randArray = [1,2,3,-3,'arrrr','book'];
+
+        // console.log(randArray.length);
+
+        // randArray.forEach(element => {
+        //     console.log(element);
+
+        // });
+
+        // const mapArray = randArray.map(x => console.log(x));
+
+        /* Задание 3. раздела 5.3
+        Дана строка. Необходимо вывести в консоль перевёрнутый вариант. Например, "Hello" -> "olleH". */
+
+        // const inString = 'Hello';
+        // let outString = '';
+
+        // console.log(inString);
+
+        // for (let i=inString.length-1; i >= 0 ; i--) {
+        //     outString = outString + inString[i];
+        // } 
+
+        // console.log(outString);
+
+
+        /* Задание 4. раздела 5.3
+           Записать в переменную случайное целое число в диапазоне [0; 100]. Используйте объект Math.*/
+        // let randomNumber = Math.trunc(Math.random()*100);
+        // console.log(`Случайное число: ${randomNumber}`); 
+
+
+        /* задание 2 раздела 5.3 
+
+        let inputValue;
+        inputValue = true;
+        // inputValue = 'dssdf';
+        // inputValue = 20;
+        // inputValue = null;
+
+
+        switch (typeof(inputValue)) {
+            case 'string':
+                console.log(inputValue + ' - string');
+                break;
+            case 'boolean':
+                console.log(inputValue + ' - boolean');
+                break;
+            case 'number':
+                console.log(inputValue + ' - number');
+                break;
+            default:
+                console.log(inputValue + ' - unknown');    
         }
-    }
 
-*/
-
-
-/* Задание 5
-
-const NDS  = 0.20;
-const NDFL = 0.13;
-const EXCISE = 0.1;
-const RENT = 40000;
-const FOOD = 15000;
-const OTHER = 15000;
-const MY_SALARY = 120000;
-let ndflTax;
-let mySalaryNet;
-let otherTaxes;
-let totalAvailable;
+        */
 
 
-ndflTax = MY_SALARY * NDFL;
-mySalaryNet = MY_SALARY - ndflTax;
-otherTaxes = mySalaryNet * (NDS + EXCISE);
-totalAvailable = mySalaryNet - otherTaxes;
-totalAvailable -= FOOD + OTHER + RENT
-alert('Зарплата ' + MY_SALARY + ' рублей')
-alert('Уплачено налогов на сумму ' + (ndflTax + otherTaxes) + ' рублей')
-alert('Осталось ' + totalAvailable + ' рублей')
-*/
 
-/* Задание 4 
-a = 12;
-b = 'number';
-c = false;
-d = null;
-e = undefined;
-f = 123.34;
-g = '1' + 1;
-h = 15 / 0;
-i = -'5';
-j = 5 == '5'
 
-at = typeof a === 'number'; // true
-bt = typeof b === 'string'; // false, а нужно чтобы все были true
-ct = typeof c === 'boolean';
-dt = typeof d === 'object';
-et = typeof e === 'undefined';
-ft = typeof f === 'number';
-gt = typeof g === 'string';
-ht = typeof h === 'number';
-it = typeof i === 'number';
-jt = typeof j === 'boolean';
+        /* задание 1 раздел 5.3
+        const inputString = prompt('Введите число:');
+        const castInputValue = +inputString;
 
-alert(at && bt && ct && dt && et && ft && gt && ht && it && jt);
-*/
+        if (isNaN(castInputValue)) {
+            console.log('Упс, кажется вы ошиблись, "' + inputString + '" не является числом' );
+        } else  {
+                const resultOdd = (castInputValue % 2 === 0);
+                if (castInputValue % 2 === 0) {
+                    console.log('Число ' + inputString + ' - четное')
+                }  else {
+                    console.log('Число ' + inputString + ' - нечетное');
+                }
+            }
 
-/* Задание 3 
-let x = prompt('Введите занчение X');
-let x2 = Math.pow(x,2);
-let x3 = Math.pow(x,3);
-let val = Math.round(((((4*x2) + 18 - (23*x))/(5/9*x + 18*x3/33*x))+x*15/12)*10000)/10000;
-alert('Результат расчета формулы = ' + val);
-*/
+        */
 
-/* Задание 2 
-let cTemp = ;
-alert(cTemp + ' градусов по Цельсию равны ' + ((cTemp*9/5)+32) + ' градусам по Фаренгейту.');
 
-или 
+        /* Задание 5
 
-alert(prompt('Введите значение температуры в C') + ' градусов по Цельсию равны ' + ((cTemp*9/5)+32) + ' градусам по Фаренгейту.'); */
+        const NDS  = 0.20;
+        const NDFL = 0.13;
+        const EXCISE = 0.1;
+        const RENT = 40000;
+        const FOOD = 15000;
+        const OTHER = 15000;
+        const MY_SALARY = 120000;
+        let ndflTax;
+        let mySalaryNet;
+        let otherTaxes;
+        let totalAvailable;
+
+
+        ndflTax = MY_SALARY * NDFL;
+        mySalaryNet = MY_SALARY - ndflTax;
+        otherTaxes = mySalaryNet * (NDS + EXCISE);
+        totalAvailable = mySalaryNet - otherTaxes;
+        totalAvailable -= FOOD + OTHER + RENT
+        alert('Зарплата ' + MY_SALARY + ' рублей')
+        alert('Уплачено налогов на сумму ' + (ndflTax + otherTaxes) + ' рублей')
+        alert('Осталось ' + totalAvailable + ' рублей')
+        */
+
+        /* Задание 4 
+        a = 12;
+        b = 'number';
+        c = false;
+        d = null;
+        e = undefined;
+        f = 123.34;
+        g = '1' + 1;
+        h = 15 / 0;
+        i = -'5';
+        j = 5 == '5'
+
+        at = typeof a === 'number'; // true
+        bt = typeof b === 'string'; // false, а нужно чтобы все были true
+        ct = typeof c === 'boolean';
+        dt = typeof d === 'object';
+        et = typeof e === 'undefined';
+        ft = typeof f === 'number';
+        gt = typeof g === 'string';
+        ht = typeof h === 'number';
+        it = typeof i === 'number';
+        jt = typeof j === 'boolean';
+
+        alert(at && bt && ct && dt && et && ft && gt && ht && it && jt);
+        */
+
+        /* Задание 3 
+        let x = prompt('Введите занчение X');
+        let x2 = Math.pow(x,2);
+        let x3 = Math.pow(x,3);
+        let val = Math.round(((((4*x2) + 18 - (23*x))/(5/9*x + 18*x3/33*x))+x*15/12)*10000)/10000;
+        alert('Результат расчета формулы = ' + val);
+        */
+
+        /* Задание 2 
+        let cTemp = ;
+        alert(cTemp + ' градусов по Цельсию равны ' + ((cTemp*9/5)+32) + ' градусам по Фаренгейту.');
+
+        или 
+
+        alert(prompt('Введите значение температуры в C') + ' градусов по Цельсию равны ' + ((cTemp*9/5)+32) + ' градусам по Фаренгейту.'); */
